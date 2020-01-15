a2ff13eaffca1990af7e1d395accd636ae741323
Leonardo Kanashiro <leonardo.k.kanashiro@gmail.com>
2020-01-15 17:41:07 -0300

    Criacao do backend, contendo a listagem(index), armazenamento(store) e a busca(search)

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Leo:Passdb20@cluster0-iwydf.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+//Métodos HTTP: get, post, put, delete
+
+//Tipos de parâmetros:
+//Query Params: request.query (Filtros, ordenação, paginação, ...)
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de registro)
+
+//MongoDB (Não-relacional)
+
+app.listen(3333);
