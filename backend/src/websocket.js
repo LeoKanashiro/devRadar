6871ebf30e1df10dc7cc1c6d0fb24ddbeb84f40b
Leonardo Kanashiro <leonardo.k.kanashiro@gmail.com>
2020-01-17 16:57:32 -0300

    Mobile real time

---
+++ backend/src/websocket.js
@@ -0,0 +1,44 @@
+const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
+
+let io;
+const connections = [];
+
+exports.setupWebsocket = (server) => {
+    io = socketio(server);
+
+    io.on('connection', socket => {
+        
+        const { latitude, longitude, techs } = socket.handshake.query;
+
+        connections.push({
+            id: socket.id,
+            coordinates: {
+                latitude: Number(latitude),
+                longitude: Number(longitude)
+            },
+            techs: parseStringAsArray(techs)
+        });
+
+        //TESTE para verificar se há a comunicação em tempo real
+        //setTimeout(() => {
+        //    socket.emit('message', 'Hello')
+        //}, 3000);
+    });
+};
+
+//Verifica as condições de tecnologias e na distancia de até 10km
+
+exports.findConnections = (coordinates, techs) => {
+    return connections.filter(connection => {
+        return calculateDistance(coordinates, connection.coordinates) < 10
+        && connection.techs.some(item => techs.includes(item))
+    })
+}
+
+exports.sendMessage = (to, message, data) => {
+    to.forEach(connection => {
+        io.to(connection.id).emit(message, data);
+    });
+}
